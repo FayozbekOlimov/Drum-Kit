078406a92b0cbe242351b1848d00c991085eb9d5
FayozbekOlimov <Fayozbek2021dev@gmail.com>
2021-07-15 09:12:49 +0500

    All files loaded

---
+++ index.js
@@ -0,0 +1,55 @@
+const drum = document.querySelectorAll(".drum");
+
+for (let i = 0; i < drum.length; i++) {
+    drum[i].addEventListener("click", () => {
+        const textBtn = drum[i].textContent;
+        play(textBtn);
+        animation(textBtn);
+    })
+}
+
+document.addEventListener("keypress", (e) => {
+    play(e.key);
+    animation(e.key);
+})
+
+function play(key) {
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            let tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            let tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            let tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            let kickBass = new Audio('./sounds/kick-bass.mp3');
+            kickBass.play(); break;
+        case 'k':
+            let crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'l':
+            let snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+    }
+}
+
+function animation(key) {
+    const drum = document.querySelector(`.${key}`);
+    drum.classList.add('pressed');
+  
+    setTimeout(function () {
+      drum.classList.remove('pressed');
+    }, 100)
+}
